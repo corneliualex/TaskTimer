c869fcc80249972c84b5995289f1dab8126adb05
pcorneliu <corneliu@rubrikkgroup.com>
2018-05-22 15:10:43 +0300

    Added basic mvc web app project into master

---
+++ TaskTimer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskTimer.Startup))]
+namespace TaskTimer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
